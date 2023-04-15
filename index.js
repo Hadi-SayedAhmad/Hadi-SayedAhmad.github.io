52c7ccb06fe6923d016565d3b9ce9cee8d39460f
Hadi-SayedAhmad <hadi.github.acc@gmail.com>
2023-04-15 21:50:53 +0300

    refurbushed

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
+/* ALL PAGES :: NAVIGATION :: HAMBURGER ICON FUNCTIONALITY :: JAVASCRIPT */
+
 let nav_toggle = document.querySelector(".nav_toggle");
 let nav_toggle_icon = document.querySelector(".nav_toggle ion-icon");
 let nav_menu = document.querySelector(".nav_menu");
 
-nav_toggle.addEventListener("click", () => {
+nav_toggle.addEventListener("click", () => 
+{
   nav_menu.classList.toggle("active");
   nav_toggle_icon.setAttribute(
     "name",
     nav_menu.classList.contains("active") ? "close-outline" : "menu-outline"
   );
 });
+/**********************************************************************************************************************/
 
-/*
-This code defines the filterDisplayedProducts() function that filters the displayed products based on the search input. It also adds a click event listener to the search button, which calls the filterDisplayedProducts() function when clicked.
-
-Note that this code assumes that you have HTML elements with the class product to represent your products, and that each product element has a child element with the class product-name that contains the product name. If your HTML is structured differently, you may need to adjust the code accordingly. */
 
 /* HOME PAGE :: MAIN :: SLIDER FUNCTIONALITY :: JQUERY */
 
-$(document).ready(function () {
+$(document).ready(function () 
+{
   let sliderContainer = $(".slider-container");
   let sliderItems = sliderContainer.find(".slider-item");
   let sliderItemCount = sliderItems.length;
@@ -28,7 +29,8 @@ $(document).ready(function () {
   let rightArrow = $(".slider-arrow-right");
 
   // SLIDE FUNCTIONALITY ACCORDING TO CERTAIN INDEX ARGUMENT
-  function slide(index) {
+  function slide(index) 
+  {
     // CURRENT SLIDE FUNCTION + AUTOMATIC SLIDING
     sliderItems.removeClass("active");
     $(sliderItems[index]).addClass("active");
@@ -42,69 +44,99 @@ $(document).ready(function () {
 
   let slideTimer = setInterval(nextSlide, slideInterval);
 
-  function nextSlide() {
+  function nextSlide() 
+  {
     let nextIndex = activeIndex + 1;
-    if (nextIndex >= sliderItemCount) {
+    if (nextIndex >= sliderItemCount) 
+    {
       nextIndex = 0;
     }
     slide(nextIndex);
   }
 
-  function previousSlide() {
+  function previousSlide() 
+  {
     let previousIndex = activeIndex - 1;
-    if (previousIndex < 0) {
+    if (previousIndex < 0) 
+    {
       previousIndex = sliderItemCount - 1;
     }
     slide(previousIndex);
   }
 
-  leftArrow.click(function () {
+  leftArrow.click(function () 
+  {
     previousSlide();
   });
-  rightArrow.click(function () {
+
+  rightArrow.click(function () 
+  {
     nextSlide();
   });
 });
-
 /************************************************************************************************************************/
 
-/* HOME PAGE :: MAIN :: CAROUSEL FUNCTIONALITY :: JQUERY */
+/* HOME PAGE :: MAIN :: FEATURED PRODUCTS BOX FUNCTIONALITY :: JAVASCRIPT */
+
 let filter_btn = document.querySelectorAll(".filter-btn");
 let tab_items = document.querySelectorAll(".tab-item");
 
-for (let i = 0; i < filter_btn.length; i++) {
-  filter_btn[i].addEventListener("click", function () {
-    for (let j = 0; j < filter_btn.length; j++) {
+for (let i = 0; i < filter_btn.length; i++) 
+{
+  filter_btn[i].addEventListener("click", function () 
+  {
+    for (let j = 0; j < filter_btn.length; j++) 
+    {
       filter_btn[j].classList.remove("active");
     }
+
     let select_tab = filter_btn[i].getAttribute("data-tab");
+
     filter_btn[i].classList.add("active");
-    for (let t = 0; t < tab_items.length; t++) {
+
+    for (let t = 0; t < tab_items.length; t++) 
+    {
       document.querySelector(".tab-filter-item-container").style.height =
         tab_items[t].scrollHeight + "px";
-      if (tab_items[t].classList.contains(select_tab)) {
+
+      if (tab_items[t].classList.contains(select_tab)) 
+      {
         tab_items[t].classList.add("select_tab");
-      } else {
+      } 
+      else 
+      {
         tab_items[t].classList.remove("select_tab");
       }
     }
+
   });
 }
-
-for (let th = 0; th < tab_items.length; th++) {
+for (let th = 0; th < tab_items.length; th++) 
+{
   document.querySelector(".tab-filter-item-container").style.height =
     tab_items[th].scrollHeight + "px";
 }
+/**********************************************************************************************************************/
+
+
+/* ALL PAGES :: MAIN :: BAC TO TOP BUTTON FUNCTIONALITY :: JAVASCRIPT */
 
 let back_to_top = document.querySelector(".back_to_top");
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
+
+window.addEventListener("scroll", () => 
+{
+  if (window.pageYOffset > 100) 
+  {
     back_to_top.classList.add("show");
-  } else {
+  } 
+  else 
+  {
     back_to_top.classList.remove("show");
   }
 });
-back_to_top.addEventListener("click", function (e) {
+
+back_to_top.addEventListener("click", function (e) 
+{
   window.scrollTo({
     top: 0,
     left: 0,
